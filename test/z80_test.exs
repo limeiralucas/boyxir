313743a90d79f6ddb0c77300f1b64633c8c7c5f3
Lucas Limeira <lucasalveslm@gmail.com>
2018-03-20 22:22:55 -0300

    test Z80.addr_e function

--- test/z80_test.exs
+++ test/z80_test.exs
@@ -21,4 +21,29 @@ defmodule Z80Test do
 			}
 		}
   end
+
+  test "addr_e" do
+  	state = Z80.init_state
+  	registers = %{ state[:_r] | a: 1, e: 3 }
+
+  	state = %{state | _r: Z80.addr_e(registers) }
+
+  	assert state == %{
+			_clock: %{m: 0, t: 0},
+			_r: %{
+				a: 4,
+				b: 0,
+				c: 0,
+				d: 0,
+				e: 3,
+				h: 0,
+				l: 0,
+				f: 0,
+				pc: 0,
+				sp: 0,
+				m: 0,
+				t: 0
+			}
+		}
+  end
 end
