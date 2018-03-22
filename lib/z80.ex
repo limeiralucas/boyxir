b945350d46f6f3b53b468adebc853d9ebccd1f27
Lucas Limeira <lucasalveslm@gmail.com>
2018-03-21 23:12:59 -0300

    Docs

--- lib/z80.ex
+++ lib/z80.ex
@@ -1,4 +1,13 @@
 defmodule Z80 do
+	@moduledoc """
+	Simulate the modified Zilog Z80 processor used by Gameboy
+	"""
+
+	@doc """
+	Returns a initial processor state
+
+	"""
+	@spec init_state() :: Map
 	def init_state() do
 		%{
 			_clock: %{m: 0, t: 0},
@@ -19,11 +28,28 @@ defmodule Z80 do
 		}
 	end
 
+	@doc """
+	Sum values of register A and E storing the result in register A
+
+	## Parameters
+
+	  - registers: A map representing the actual state of registers.
+
+	"""
+	@spec addr_e(Map) :: Map
 	def addr_e(registers) do
 		sum = registers[:a] + registers[:e]
 		%{registers | a: sum}
 	end
 
+	@doc """
+	Compare register B to A, setting flags
+
+	## Parameters
+	 - registers: A map representing the actual state of registers.
+
+	"""
+	@spec cpr_b(Map) :: Map
 	def cpr_b(registers) do
 		subtraction = registers[:a] - registers[:b]
 		cond do
@@ -34,10 +60,27 @@ defmodule Z80 do
 		end
 	end
 
+
+	@doc """
+	Represents No-operation (NOP) of processor
+
+	## Parameters
+	 - registers: A map representing the actual state of registers.
+
+	"""
+	@spec nop(Map) :: Map
 	def nop(registers) do
 		%{registers | m: 1, t: 4}
 	end
 
+
+	@doc """
+	Push registers B and C to the stack
+
+	## Parameters
+	 - registers: A map representing the actual state of registers.
+
+	"""
 	def pushbc(registers) do
 		t_sp = registers[:sp] - 1
 		MMU.wb(t_sp, registers[:b])
@@ -47,6 +90,13 @@ defmodule Z80 do
 		%{registers | sp: t_sp, m: 3, t: 12}
 	end
 
+	@doc """
+	Pop registers H and L off the stack
+
+	## Parameters
+	 - registers: A map representing the actual state of registers.
+
+	"""
 	def pophl(registers) do
 		t_sp = registers[:sp]
 		t_l = MMU.rb(t_sp)
@@ -57,6 +107,14 @@ defmodule Z80 do
 		%{registers | sp: t_sp, h: t_h, l: t_l, m: 3, t: 12}
 	end
 
+
+	@doc """
+	Read a byte from absolute location into A
+
+	## Parameters
+	 - registers: A map representing the actual state of registers.
+
+	"""
 	def ldamm(registers) do
 		t_pc = registers[:pc]
 		addr = MMU.rw(t_pc)
